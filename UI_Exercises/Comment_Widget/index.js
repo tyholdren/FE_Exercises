d5b7213790d9fb0d5a5a1d5ba6efa0b9d6e84982
tyholdren <tsholdre@syr.edu>
2024-03-13 08:38:32 -0400

    renaming

--- UI_Exercises/Comment_Widget/index.js
+++
@@ -1,7 +0,0 @@
-import PostFeed from './PostFeed.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM loaded');
-  const newPostFeed = new PostFeed();
-  newPostFeed.initializeFeed();
-});
