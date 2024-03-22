346d7162291bf030667896fd4578ef30724051e9
tyholdren <tsholdre@syr.edu>
2024-03-22 10:27:56 -0400

    all transfer functionality

--- UI_Exercises/Transfer_List/script.js
+++ UI_Exercises/Transfer_List/script.js
@@ -3,9 +3,6 @@ class TransferList {
     this.toMoveLeft = new Set();
     this.toMoveRight = new Set();
 
-    this.leftState = new Set();
-    this.rightState = new Set();
-
     this.moveLeftButton = document.getElementById('move-left');
     this.moveAllLeftButton = document.getElementById('move-all-left');
     this.moveRightButton = document.getElementById('move-right');
@@ -17,8 +14,15 @@ class TransferList {
     this.rightContainer = document.getElementById('right-container');
     const items = Array.from(document.getElementsByClassName('transfer-item'));
     items.forEach(item => {
-      this.rightState.add(item);
       this.rightContainer.appendChild(item);
+    });
+
+    this.moveLeftButton.addEventListener('click', () => {
+      this.moveLeft();
+    });
+
+    this.moveRightButton.addEventListener('click', () => {
+      this.moveRight();
     });
 
     this.moveAllLeftButton.addEventListener('click', () => this.moveAllLeft());
@@ -29,31 +33,49 @@ class TransferList {
     const transferList = document.getElementById('transfer-list-container');
     transferList.addEventListener('click', event => {
       if (event.target.tagName === 'INPUT') {
-        if (this.leftState.has(event.target.parentNode)) {
+        if (event.target.parentNode.parentNode.id === 'left-container') {
           if (event.target.checked === true) {
             this.toMoveRight.add(event.target.parentNode);
           } else {
             this.toMoveRight.delete(event.target.parentNode);
           }
-        } else if (this.rightState.has(event.target.parentNode)) {
+        } else if (
+          event.target.parentNode.parentNode.id === 'right-container'
+        ) {
           if (event.target.checked === true) {
             this.toMoveLeft.add(event.target.parentNode);
           } else {
             this.toMoveLeft.add(event.target.parentNode);
           }
         }
-        console.log('left state', this.leftState);
-        console.log('right state', this.rightState);
       }
     });
+  }
+
+  moveLeft() {
+    const items = Array.from(this.toMoveLeft);
+    items.forEach(item => {
+      const inputBox = item.querySelector('input');
+      inputBox.checked = false;
+      this.leftContainer.appendChild(item);
+    });
+    this.toMoveLeft = new Set();
+  }
+
+  moveRight() {
+    const items = Array.from(this.toMoveRight);
+    items.forEach(item => {
+      const inputBox = item.querySelector('input');
+      inputBox.checked = false;
+      this.rightContainer.appendChild(item);
+    });
+    this.toMoveRight = new Set();
   }
 
   moveAllLeft() {
     const items = Array.from(document.getElementsByClassName('transfer-item'));
     this.rightContainer.textContent = '';
-    this.rightState = new Set();
     items.forEach(item => {
-      this.leftState.add(item);
       this.leftContainer.appendChild(item);
     });
   }
@@ -61,9 +83,7 @@ class TransferList {
   moveAllRight() {
     const items = Array.from(document.getElementsByClassName('transfer-item'));
     this.leftContainer.textContent = '';
-    this.leftState = new Set();
     items.forEach(item => {
-      this.rightState.add(item);
       this.rightContainer.appendChild(item);
     });
   }
