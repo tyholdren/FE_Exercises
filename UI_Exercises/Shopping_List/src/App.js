e5c2c3986f7ce5e58efc74c2f4c536007b5a83a3
tyholdren <tsholdre@syr.edu>
2024-04-06 18:45:10 -0400

    shopping list app - wip

---
+++ UI_Exercises/Shopping_List/src/App.js
@@ -0,0 +1,112 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+const URL = 'https://api.frontendeval.com/fake/food/';
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      if (searchInput.length > 1) {
+        try {
+          const response = await fetch(`${URL}${searchInput}`);
+          const results = await response.json();
+          setItems(results);
+        } catch (error) {
+          console.log(new Error(error));
+        }
+      } else if (searchInput.length === 0) {
+        setItems([]);
+      }
+    };
+    getItems();
+  }, [searchInput]);
+
+  const updateInput = event => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleDropdownClick = event => {
+    const item = event.target.getAttribute('data-item-id');
+    const newItem = { id: list.length, item: item };
+    setList([...list, newItem]);
+  };
+
+  const deleteItem = idToDelete => {
+    const newList = list.filter(({ id }) => id !== idToDelete);
+    setList(newList);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <label htmlFor="shopping-list-input">Search for Items</label>
+        <input
+          id="shopping-list-input"
+          name="shopping-list-input"
+          type="text"
+          value={searchInput}
+          onChange={updateInput}
+        />
+      </div>
+      <div
+        className="dropdown-container"
+        onClick={event => handleDropdownClick(event)}
+      >
+        {items.map((item, index) => {
+          return (
+            <DropdownItem
+              key={`item-${index}`}
+              data-item-id={item}
+              item={item}
+            />
+          );
+        })}
+      </div>
+      <div className="list-container">
+        {list.map(({ id, item }) => {
+          return (
+            <ListItem
+              key={`item-${id}`}
+              id={id}
+              item={item}
+              deleteItem={deleteItem}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function DropdownItem({ item }) {
+  return (
+    <div className="dropdown-item">
+      <button data-item-id={item}>{item}</button>
+    </div>
+  );
+}
+
+function ListItem({ id, item, deleteItem }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const toggleChecked = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div className={isChecked ? 'selected-list-item' : 'list-item'}>
+      <input
+        checked={isChecked}
+        onChange={() => toggleChecked()}
+        type="checkbox"
+        id={`item-${item}`}
+      />
+      <label htmlFor={`item-${item}`}>{item}</label>
+      <button onClick={() => deleteItem(id)}>X</button>
+    </div>
+  );
+}
